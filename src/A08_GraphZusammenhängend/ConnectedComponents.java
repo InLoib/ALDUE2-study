8c9199f9208f57a74552c82853669bffaad2e68a
InLoib <ina.loibner@edu.campus02.at>
2017-09-17 21:09:47 +0200

    GraphZusammenhängend geschafft
    JUinit funktioniert
    !! Voraussetzung:
    - Knoten immer Zahlen
    - beginnend mit 0
    - fortlaufende Nummerierung

--- src/A08_GraphZusammenhängend/ConnectedComponents.java
+++ src/A08_GraphZusammenhängend/ConnectedComponents.java
@@ -1,8 +1,15 @@
 package A08_GraphZusammenhängend;
 
+import java.util.ArrayDeque;
+import java.util.List;
+
 import basisAlgorithmen.Graph;
+import basisAlgorithmen.WeightedEdge;
 
 public class ConnectedComponents {
+	
+	private Graph graph;
+	private boolean[] besuchteKnotenListe;
 	
 	/**
 	 * Retourniert die Anzahl der zusammenhängenden Komponenten eines Graphen
@@ -10,8 +17,36 @@ public class ConnectedComponents {
 	 * @return Anzahl der Komponenten
 	 */
 	public int getNumberOfComponents(Graph g) {
-
-		return 0;
+		
+		int returnCounter = 0;
+		graph = g;
+		int anzahlKnoten = graph.numVertices();
+		besuchteKnotenListe = new boolean[anzahlKnoten];
+		
+		for (int i = 0; i < anzahlKnoten; i++) {
+			if(besuchteKnotenListe[i] == false){
+				returnCounter++;
+							
+				ArrayDeque<Integer> stack = new ArrayDeque<>();
+				stack.push(i);
+							
+				while(stack.isEmpty() != true){
+					Integer actualVertec = stack.pop();
+					besuchteKnotenListe[actualVertec] = true;
+					List<WeightedEdge> gefundeneEdges = graph.getEdges(actualVertec);
+					
+					for (WeightedEdge weightedEdge : gefundeneEdges) {
+						if(besuchteKnotenListe[weightedEdge.to_vertex] == false){
+							stack.push(weightedEdge.to_vertex);
+						}
+					}
+					
+				}
+			}
+			
+		}
+			
+		return returnCounter;
 	}
 
 }

e251ce7680f3a2461aeb752e94f81685c506cc5f
InLoib <ina.loibner@edu.campus02.at>
2017-09-18 14:12:28 +0200

    Dijkstra funtioniert nichts
    ...statt mit Knoten mit den Kanten zu arbeiten kann ich nicht....

--- Klausur_Versuch1/dijkstra/Dijkstra.java
+++ Klausur_Versuch1/dijkstra/Dijkstra.java
@@ -1,5 +1,6 @@
 package dijkstra;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +9,46 @@ public class Dijkstra {
 	public static List<Integer> dijkstra(Graph g, int von, int nach) {
 
 		//TODO: Hier sollte Ihre Implementierung sein
+
 		
-		return null; // <-- Liefern Sie hier ihr Ergebnis retour
+		List<Integer> returnList = new ArrayList<>();
+		
+		int anzahlKnoten = g.numVertices();
+		int[] vorgaengerKnoten = new int[anzahlKnoten];
+		int[] kosten = new int[anzahlKnoten];
+		
+		for (int i = 0; i < anzahlKnoten; i++) {
+			vorgaengerKnoten[i] = -1;
+			kosten[i] = 999999;
+		}
+		
+		
+		List<WeightedEdge> listeKanten = new ArrayList<>();
+		ArrayDeque<WeightedEdge> queue = new ArrayDeque<>();
+		for (int i = 0; i < anzahlKnoten; i++) {
+			listeKanten = g.getEdges(i);
+		}
+		for (WeightedEdge weightedEdge : listeKanten) {
+			
+			if(queue.contains(weightedEdge) != true){
+				queue.add(weightedEdge);
+			}	
+		}
+		
+		VertexHeap heap = new VertexHeap(anzahlKnoten);
+		for (int i = 0; i < queue.size(); i++) {
+			heap.insert(queue.poll());
+		}
+		
+		while(heap.isEmpty() != false){
+			
+		}
+		
+		
+		
+		
+		
+		return returnList; // <-- Liefern Sie hier ihr Ergebnis retour
 		
 	}
 	

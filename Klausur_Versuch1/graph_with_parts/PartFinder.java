dd18d9b549d2e77f997f081282f0564cb65ff29d
InLoib <ina.loibner@edu.campus02.at>
2017-09-18 14:32:32 +0200

    Graph funktioniert
    yey

--- Klausur_Versuch1/graph_with_parts/PartFinder.java
+++ Klausur_Versuch1/graph_with_parts/PartFinder.java
@@ -1,7 +1,10 @@
 package graph_with_parts;
 
+import java.util.ArrayDeque;
+import java.util.List;
 
 public class PartFinder {
+	
 	
 	public int calcPartCount(ArrayGraph g) {
 		
@@ -12,7 +15,41 @@ public class PartFinder {
 		// Denken Sie daran, dass Sie jederzeit Hilfsmethoden
 		// machen können.
 		
-		return 0; // <-- Vergessen Sie nicht, Ihr Ergebnis zu retournieren!
+		
+		int knotenAnzahl = g.numVertices();
+
+		int returnValue = 0;
+		
+		for (int i = 0; i < knotenAnzahl; i++) {
+			if(visited[i] == false){
+				returnValue++;
+			}
+			
+			ArrayDeque<Integer> stack = new ArrayDeque<>();
+			stack.push(i);
+			
+			while(stack.isEmpty() == false){
+				int actualerKnoten = stack.pop();
+				visited[actualerKnoten] = true;
+				
+				List<WeightedEdge> listederKantenVomActualKnoten = g.getEdges(actualerKnoten);
+				
+				for (WeightedEdge weightedEdge : listederKantenVomActualKnoten) {
+					if(visited[weightedEdge.vertex] == false){
+						stack.push(weightedEdge.vertex);
+					}
+				}
+				
+				
+				
+			}
+			
+			
+		}
+		
+		
+		
+		return returnValue; // <-- Vergessen Sie nicht, Ihr Ergebnis zu retournieren!
 
 	}
 	

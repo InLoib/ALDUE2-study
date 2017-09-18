147cf98707788ab3819542caab05d685a32af686
InLoib <ina.loibner@edu.campus02.at>
2017-09-18 23:57:51 +0200

    Dijkstra eine Idee & Graph Versuch 2

--- Klausur_Versuch1/dijkstra/Dijkstra.java
+++ Klausur_Versuch1/dijkstra/Dijkstra.java
@@ -8,9 +8,46 @@ public class Dijkstra {
 	public static List<Integer> dijkstra(Graph g, int von, int nach) {
 
 		//TODO: Hier sollte Ihre Implementierung sein
+
+
+		int[] vorgaenger = new int[g.numVertices()];
+		int[] distanz = new int[g.numVertices()];
 		
-		return null; // <-- Liefern Sie hier ihr Ergebnis retour
+		for (int i = 0; i < g.numVertices(); i++) {
+			vorgaenger[i] = -1;
+			distanz[i] = 9999;
+		}
 		
+		distanz[von] = 0;
+		
+		VertexHeap heap = new VertexHeap(g.numVertices());
+		for (int i = 0; i < g.numVertices(); i++) {
+			heap.insert(new WeightedEdge(i, distanz[i]));
+		}
+		
+		while(heap.isEmpty() != true){
+			WeightedEdge actuell = heap.remove();
+			
+			List<WeightedEdge> KantenDesActuell = g.getEdges(actuell.vertex);
+			
+			for (WeightedEdge perKante : KantenDesActuell) {
+				if(heap.contains(perKante)){
+					
+					int dist = distanz[actuell.vertex] + perKante.weight;
+					
+					if(dist < distanz[actuell.vertex]){
+						distanz[actuell.vertex] = dist;
+						vorgaenger[actuell.vertex] = actuell.vertex;
+						
+						heap.setPriority(actuell.vertex, dist);
+					}					
+				}				
+			}	
+		}
+		
+		List<Integer> way = predToWay(vorgaenger, von, nach);
+		
+		return way; // <-- Liefern Sie hier ihr Ergebnis retour
 	}
 	
 	private static ArrayList<Integer> predToWay(int[] pred, int from, int to) {
@@ -19,8 +56,13 @@ public class Dijkstra {
 		
 		// TODO: Implementierung dar ausgabe
 		
+		int current = to;
+		way.add(to);
+		
+		while(current != from){
+			way.add(0, pred[current]);
+			current = pred[current];
+		}	
 		return way;
 	}
-	
-
 }
---
+++ Klausur_Versuch1/graph_with_parts1/PartFinder.java
@@ -0,0 +1,20 @@
+package graph_with_parts1;
+
+
+public class PartFinder {
+	
+	public int calcPartCount(ArrayGraph g) {
+		
+		boolean[] visited = new boolean[g.numVertices()];
+		
+		// Hier sollte Ihr Code stehen
+		
+		// Denken Sie daran, dass Sie jederzeit Hilfsmethoden
+		// machen können.
+		
+		return 0; // <-- Vergessen Sie nicht, Ihr Ergebnis zu retournieren!
+
+	}
+	
+
+}
